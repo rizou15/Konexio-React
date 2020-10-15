96c9993e29d7354f6f8b720d7ed2f36e54173766
rizou15 <rizou1504@gmail.com>
2020-10-15 16:58:33 +0200

    exercice react _trippy tout installation

---
+++ react_trippy/src/containers/HomeContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class HomeContainer extends React {
+
+}
+
+render(){
+
+    return (
+        <div>
+            <p>Hello Home</p>
+        </div>
+
+);
+
+}
+
+
+export default HomeContainer;
